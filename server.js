32bd6bf9ad62c8ec58c38be5180726c9756c2e1f
Robert Warren <warrenrobert127@gmail.com>
2022-02-27 16:34:32 -0600

    server

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(require("./routes"));
-
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
-// logs mongo queries being executed
+// Use this to log mongo queries being executed!s
 mongoose.set("debug", true);
+
+app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
